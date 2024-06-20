af9d7f53588edc931f751646f38e3b38f85547e8
Priscilla <comptepro@mbpdepriscilla.home>
2024-06-20 13:04:00 +0200

    add package altorouter

--- public/index.php
+++ public/index.php
@@ -1,13 +1,22 @@
 <?php 
 // php -S localhost:8000 -t public
 require '../vendor/autoload.php';
-require '../elements/header.php';
 $uri = $_SERVER['REQUEST_URI'];
-if ($uri === '/nous-contacter') {
-    require '../templates/contact.php';
-} elseif ($uri === '/') {
-    require '../templates/home.php';
-} else {
-    echo 404;
+$router = new AltoRouter();
+$router->map('GET', '/', function () {
+    echo 'Salut';
+});
+$router->map('GET', '/nous-contacter', function () {
+    echo 'Nous contacter';
+});
+$router->map('GET', '/blog/[*:slug]-[i:id]', function ($slug, $id) {
+    echo "Je suis sur l'article $slug avec le numéro $id";
+});
+$match = $router->match();
+if ($match !== null) {
+    require '../elements/header.php';
+    call_user_func_array($match['target'],$match['params']);
+    require '../elements/footer.php';
 }
-require '../elements/footer.php';
+
+
